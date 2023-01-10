b4f88c95fa406c6c706be2aa0d48f7a40053d2ff
Fitrah Elly Firdaus <fitrah.firdaus@gmail.com>
2023-01-10 10:30:44 +0700

    Init Project

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+<<<<<<< HEAD
 use App\Http\Controllers\API\ProductApiController;
+=======
+>>>>>>> 1c25b44 (Init Project)
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+<<<<<<< HEAD
 
 Route::group(['prefix' => 'v1'], function () {
   Route::resource('products', ProductApiController::class);
 });
+=======
+>>>>>>> 1c25b44 (Init Project)

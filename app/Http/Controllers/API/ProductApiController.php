ef85928ec0781316d050cddb1387de8e36912e51
Fitrah Elly Firdaus <fitrah.firdaus@gmail.com>
2023-01-23 11:52:10 +0700

    Store_product_with_tdd

---
+++ app/Http/Controllers/API/ProductApiController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateProductRequest;
+use App\Models\Products;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class ProductApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CreateProductRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateProductRequest $request)
+    {
+        $input = $request->all();
+        $input['web_id'] =Str::uuid()->toString();
+        Log::info(print_r($input, true));
+        return response()->json(['data' => Products::create($input)], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Products $products)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Products $products)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Products $products)
+    {
+        //
+    }
+}

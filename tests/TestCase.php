2c158b2e0a92d8022b4f6d1190e7f407e62fa3fe
fitrah elly firdaus <fitrah.firdaus@gmail.com>
2023-01-26 10:52:00 +0700

    Remove Import

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Faker\Factory as Faker;
 
